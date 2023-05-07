5260f027e11bd223fd8ca76e7d701f57b65f6e5c
vodovskoff <vodovskov.ea@students.dvfu.ru>
2023-05-07 22:07:40 +1000

    Добавить метод создания постов

--- Server/src/Controller/PostController.php
+++ Server/src/Controller/PostController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Post;
 use App\Entity\User;
 use App\Previewer\PostPreviewer;
 use App\Repository\PostRepository;
-use http\Env\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -15,10 +16,12 @@ class PostController extends ApiController
 {
     private PostRepository $postRepository;
     private PostPreviewer $postPreviewer;
-    public function __construct(PostRepository $postRepository, PostPreviewer $postPreviewer)
+    private EntityManagerInterface $entityManager;
+    public function __construct(PostRepository $postRepository, PostPreviewer $postPreviewer, EntityManagerInterface $entityManager)
     {
         $this->postRepository = $postRepository;
         $this->postPreviewer = $postPreviewer;
+        $this->entityManager = $entityManager;
     }
     #[Route('/{postId}', name: 'get_post_by_id', requirements: ['postId' => '\w+'], methods: ['GET'])]
     public function getById(string $postId): JsonResponse
@@ -41,6 +44,40 @@ class PostController extends ApiController
             } else {
                 return $this->response($this->postPreviewer->previewAsNotAuthor($post));
             }
+        } catch (\Exception $exception) {
+            return $this->respondWithErrors($exception);
+        }
+    }
+
+    #[Route('/', name: 'create_post', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        try {
+            if (!$this->getUser()) {
+                return $this->respondWithErrors("You need to authenticate");
+            }
+
+            $decodedRequest = json_decode($request->getContent());
+            if ($decodedRequest->customId && $this->postRepository->findOneBy(["customId"=>$decodedRequest->customId])) {
+                return $this->respondValidationError("Post with this customId already exists");
+            }
+
+            $post = new Post();
+            $post->setAuthor($this->getUser());
+
+            $post->setCreationDate(new \DateTime("now"));
+            $post->setCustomId($decodedRequest->customId);
+            $post->setDescriptionText($decodedRequest->descriptionText);
+            $post->setIsAnonymous($decodedRequest->isAnonymous);
+            $post->setIsEncrypted($decodedRequest->isEncrypted);
+            $post->setIsReachableById($decodedRequest->isReachableById);
+            $post->setMainText($decodedRequest->mainText);
+            $post->setPublicationDate(new \DateTime($decodedRequest->publicationDate));
+
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+            return $this->respondWithSuccess("Post added successfully");
+
         } catch (\Exception $exception) {
             return $this->respondWithErrors($exception);
         }
